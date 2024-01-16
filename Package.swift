f8f18807e1220b1902575cb39d36eeca1218f1ae
gzerad <dfedoseyev@gmail.com>
2024-01-16 13:24:14 +0200

    1.4.2-circle Release

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "HMSSDK",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "HMSSDK",
+            targets: ["HMSSDK", "WebRTC", "HMSSDKDependencies"])
+    ],
+    dependencies: [
+        .package(name: "HMSAnalyticsSDK", url: "https://github.com/100mslive/100ms-ios-analytics-sdk", from: "0.0.2"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "HMSSDK",
+            url: "https://github.com/100mslive/100ms-ios-sdk-staging/releases/download/1.4.2-circle/HMSSDK.xcframework.zip",
+            checksum: "2018845a33461c23cbb9c4519cc8ddbc678995f9bb2a7d763a0f9eb533a089b6"
+        ),
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/100mslive/webrtc-ios/releases/download/1.0.4898/WebRTC.xcframework.zip",
+            checksum: "cf744aa15fa77b9b02151d1ed34c739f5bd72574664373da484d42c338f94d2e"
+        ),
+        .target(name: "HMSSDKDependencies", dependencies: ["HMSAnalyticsSDK"], path: "dependencies")
+    ]
+)
