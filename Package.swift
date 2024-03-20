7e5723d16586417cc40691121cdc36b929ee45ce
Pawan Dixit <103245157+pawan-100ms@users.noreply.github.com>
2024-03-20 17:16:32 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HMSSDK",
-            url: "https://github.com/100mslive/100ms-ios-sdk-staging/releases/download/1.8.2/HMSSDK.xcframework.zip",
-            checksum: "0542c6fe9e7ac03e2b03e5019eafc959babf3459a83d0f7400544e7434df2edd"
+            url: "https://github.com/100mslive/100ms-ios-sdk-staging/releases/download/1.8.3/HMSSDK.xcframework.zip",
+            checksum: "53764bc1a83958f4046a82b7ec483df72c96846942abe768a3c12fa585b13b66"
         ),
         .binaryTarget(
             name: "WebRTC",
